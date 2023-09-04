efc7ac4dc773a402e5004780aca4d81b71599ac2
Mitu Adrian <adrianmitu83@gmail.com>
2023-09-04 15:13:30 +0300

    make game work as wanted to + add style.css

--- script.js
+++ script.js
@@ -38,33 +38,109 @@ function playRound(playerSelection, computerSelection) {
     }
   }
 
-  let userPoints = 0;
-  let computerPoints = 0;
-  let numberOfGames = 0 ;
+let userPoints = 0;
+let computerPoints = 0;
 
-  function game(){
-        let playerChoice = prompt('Make your choice: ', 'rock');
-        let computerChoice = getComputerChoice();
-        let result = playRound(playerChoice, computerChoice);
-        if(result == 0){
-            computerPoints++;
-            return userPoints + " - " + computerPoints;
-        }
-        else if(result == 2){
-            userPoints++;
-            return userPoints + " - " + computerPoints;
+const container = document.querySelector('#main-container');
 
-        }else if(result == 1){
-            return userPoints + " - " + computerPoints;
-        }else{
-            console.log("Wrong input! Please, try again!");
-        }
+//round-message container
+const roundContent = document.createElement('div');
+roundContent.classList.add('roundContent');
+container.appendChild(roundContent);
 
-  }
+//result container
+const resultContent = document.createElement('div');
+resultContent.classList.add('resultContent');
+resultContent.textContent = userPoints.toString() + '-' + computerPoints.toString();
+container.appendChild(resultContent);
 
-  while(numberOfGames < 5){
-    numberOfGames++;
-    console.log(game());
-  }
+//end-game message-container
+const endMessage = document.createElement('div');
+endMessage.classList.add('endMessage');
+endMessage.textContent ='Who is going to be the winner?';
+container.appendChild(endMessage);
+
+function winRound(){
+roundContent.textContent = 'You win!';
+    userPoints++;
+    resultContent.textContent = userPoints.toString() + '-' + computerPoints.toString();
+}
+
+function loseRound(){
+    roundContent.textContent = 'You lost';
+    computerPoints++;
+    resultContent.textContent = userPoints.toString() + '-' + computerPoints.toString();
+}
+
+function endGame(){
+    if(userPoints == 5 ){
+        endMessage.textContent = "You won madafaka";
+    }
+    else if(computerPoints == 5){
+        endMessage.textContent = "You lost madafaka"
+    }else{
+        //endMessage.textContent ='Who is going to be the winner?';
+    }
+
+    userPoints = 0;
+    computerPoints = 0;
+    resultContent.textContent = userPoints.toString() + '-' + computerPoints.toString(); 
+}
+
+function checkEndGame(){
+    if(userPoints == 5 || computerPoints == 5){
+        endGame();
+    }
+    else if(userPoints == 0 || computerPoints == 0){
+        endMessage.textContent ='Who is going to be the winner?';
+    }
+}
+
+const rockButton = document.querySelector('#rock-button');
+rockButton.addEventListener('click', () => {
+    //endMessage.textContent ='Who is going to be the winner?';
+    const roundResult = playRound("rock", getComputerChoice());
+    if(roundResult == 2){
+        winRound();
+    }else if(roundResult == 0){
+        loseRound();
+    }else{
+        roundContent.textContent = "It's a draw!";
+    }
+
+    checkEndGame();
+});
+
+const paperButton = document.querySelector('#paper-button');
+paperButton.addEventListener('click', () => {
+    //endMessage.textContent ='Who is going to be the winner?';
+    const roundResult = playRound("paper", getComputerChoice());
+    if(roundResult == 2){
+        winRound();
+    }else if(roundResult == 0){
+        loseRound();
+    }else{
+        roundContent.textContent = "It's a draw!";
+    }
+
+    checkEndGame();
+});
+
+const scissorsButton = document.querySelector('#scissors-button');
+scissorsButton.addEventListener('click', () => {
+    //endMessage.textContent ='Who is going to be the winner?';
+    const roundResult = playRound("scissors", getComputerChoice());
+    if(roundResult == 2){
+        winRound();
+    }else if(roundResult == 0){
+        loseRound();
+    }else{
+        roundContent.textContent = "It's a draw!";
+    }
+
+    checkEndGame();
+});
+
+
 
   
